d7f0e584d2ea19f8bf053c0b40a311ee564e1f76
yangzl <youngzil@163.com>
2021-01-16 19:31:20 +0800

    feat(网关): 网关项目

---
+++ api-gateway-boot/src/main/java/org/api/gateway/boot/ApiGatewayBootstrap.java
@@ -0,0 +1,14 @@
+package org.api.gateway.boot;
+
+/**
+ * @author yangzl
+ * @description TODO
+ * @createTime 2020/7/9 01:07
+ */
+public class ApiGatewayBootstrap {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
